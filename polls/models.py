bc768690e117c9d8339bcbba01aa260e497715a9
Paul Depperschmidt <depper.paul@gmail.com>
2025-07-15 16:01:55 -0600

    end of section 7

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -7,6 +8,12 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
